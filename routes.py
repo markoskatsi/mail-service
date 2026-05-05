391b878b725ce5bfae3c21b9f6ed0bf9ffbc8902
Markos Katsi <markoskatsi05@gmail.com>
2026-05-05 13:36:00 +0100

    Added send cv endpoint

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from pydantic import EmailStr
 from starlette.responses import JSONResponse
 from fastapi_mail import FastMail, MessageSchema, MessageType
 from config import conf
@@ -6,8 +7,9 @@ from schemas import EmailRequest
 
 router = APIRouter()
 
-@router.post("/email")
-async def send_email(request: EmailRequest) -> JSONResponse:
+
+@router.post("/contact")
+async def send_contact_message(request: EmailRequest) -> JSONResponse:
     auto_reply = f"""<div style="font-family: system-ui, sans-serif, Arial; font-size: 16px;">
                         <p>Hi {request.name},</p>
                         <p>Thank you for reaching out! I've received your message, and will get back to you as soon as I can.</p>
@@ -31,6 +33,19 @@ async def send_email(request: EmailRequest) -> JSONResponse:
 
     fm = FastMail(conf)
     await fm.send_message(messages)
-    return JSONResponse(
-        status_code=200, content={"status": 200, "message": "email has been sent"}
+    return JSONResponse(content={"message": "email has been sent"})
+
+
+@router.post("/cv")
+async def send_cv(email: EmailStr) -> JSONResponse:
+    message = MessageSchema(
+        subject="CV Request",
+        recipients=[email],
+        body="<p>Hi,</p><p>Please find attached my CV.</p><p>Best regards,<br>Markos Katsi</p>",
+        subtype=MessageType.html,
+        attachments=["./assets/MarkosKatsiCV.pdf"],
     )
+
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    return JSONResponse(content={"message": "CV has been sent"})
